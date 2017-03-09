6bcf96d8038f854095883d941257f5a36ae5f023
DESKTOP-UPT3L84\quyet <quyetdv155214@gmail.com>
2017-03-09 15:23:06 +0700

    edit

--- mlab.py
+++ mlab.py
@@ -1,12 +1,12 @@
 import mongoengine
 
 
-#mongodb://<dbuser>:<dbpassword>@ds121980.mlab.com:21980/a7-dummy
+# mongodb://<dbuser>:<dbpassword>@ds121980.mlab.com:21980/myfirstapi
 host = "ds121980.mlab.com"
 port = 21980
-db_name = "a7-dummy"
-username = "admin"
-password = "admin"
+db_name = "myfirstapi"
+username = "quyetdv155214"
+password = "ba@3athuong"
 def connect():
     mongoengine.connect(db_name, host=host, port=port, username=username, password=password)
 def list2json(l):
